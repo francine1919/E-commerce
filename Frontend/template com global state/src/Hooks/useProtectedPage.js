025a73562467199f4935afb94838b479e25fcb69
francine1919 <francine1919@gmail.com>
2022-06-15 22:12:43 -0300

    add timeout for token

--- Frontend/template com global state/src/Hooks/useProtectedPage.js
+++ Frontend/template com global state/src/Hooks/useProtectedPage.js
@@ -2,20 +2,16 @@ import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../Global/GlobalContext/GlobalContext";
 
-export const useProtectedPage = (path) => {
+export const useProtectedPage = () => {
   const navigate = useNavigate();
-
 
   //Controlling the access
   useEffect(() => {
+    setTimeout(() => (localStorage.removeItem("token"), 3600000));
     const token = localStorage.getItem("token");
     if (token === null) {
       alert("Faça o cadastro para continuar.");
       navigate("/signup");
     }
-    // if (isTokenExpired) {
-    //   // alert("Seu token de acesso expirou, por favor logue-se novamente");
-    //   navigate("/signup");
-    // }
   }, []);
 };
